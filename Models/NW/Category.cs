7e4eec3289ebdb984195c6b3d849b00ecabf4822
Leanne Holmes <lholmes24@my.bcit.ca>
2023-03-31 00:19:19 -0700

    first commit

---
+++ Models/NW/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DbFirstMVC.Models.NW;
+
+public partial class Category
+{
+    [Display(Name ="ID")]
+    public int CategoryId { get; set; }
+
+    [MaxLength (15, ErrorMessage = "The max number of characters is {1}.")]
+    [Display(Name ="Category")]
+    public string CategoryName { get; set; } = null!;
+
+    [Required]
+    public string? Description { get; set; }
+
+    public byte[]? Picture { get; set; }
+
+    public virtual ICollection<Product> Products { get; } = new List<Product>();
+}
